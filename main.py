d3f06925429c6be781ba2638f252c6b3dd47e851
hanhanchan <1052718595@qq.com>
2024-01-10 14:55:26 +0800

    add controller utils

--- main.py
+++ main.py
@@ -26,11 +26,11 @@ def parse_args():
 if __name__ == "__main__":
     # or multiple clients?
     args = parse_args()
-    if args.c == 0:
-        if args.s == 1:
+    if args['c'] == 0:
+        if args['s'] == 1:
             s = SocketServer()
             s.serve()
-        if args.s == 2:
+        elif args['s'] == 2:
             s = SocketProxy()
             s.serve()
     else:
